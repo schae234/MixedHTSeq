8ee2f68b959025bef86efad7069ad699fffdd3bc
Rob <schae234@umn.edu>
2015-06-29 11:44:30 -0500

    added script to combine FPKM table from mixed_count into a proper table for camoco

---
+++ scripts/combine_counts.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2.7
+
+from __future__ import print_function
+
+import sys
+import pandas as pd
+
+import argparse
+
+
+def main(args):
+    pd.concat(
+        [pd.read_table(x,sep='\t') for x in args.files]
+    )\
+    .pivot('feature','sample',args.count)\
+    .to_csv(args.out,sep='\t')
+    return 0
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Combine fpkm from mixed_count script into table'
+    )
+    parser.add_argument(
+        '--files',nargs='+',help='count files from mixed_count script'
+    )
+    parser.add_argument(
+        '--count',type=str,help='which count column to use from the count file'
+    )
+    parser.add_argument(
+        '--out',type=str,help='output file name'
+    )
+    args = parser.parse_args()
+    # simple exit strategy
+    sys.exit(main(args))
